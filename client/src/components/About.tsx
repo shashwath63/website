a09a9d4967f6f1abb46bff0c37862b1c25376304
shashwath63 <shashwathbn10@gmail.com>
2024-01-27 10:02:53 +0530

    backend and frontend separate

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 
-const About: React.FC = () => {
+const About= () => {
   const navigate = useNavigate();
   return (
     <div className="about-page">
